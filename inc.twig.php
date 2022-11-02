eb1c4ca5907927fb19ebd77003835413bd2e098f
Hugo Latreille <hugo.latreille@gmail.com>
2022-11-02 20:00:50 +0100

    :sparkles:feat: conversion TWIG

---
+++ inc.twig.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+  'debug' => true,
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
